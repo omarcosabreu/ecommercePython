6c5834152940c036a4375947aaad413afd47e1e7
Marcos Abreu <marcosmendes.abreu7@gmail.com>
2024-02-10 19:09:45 -0300

    methods POST

--- app.py
+++ app.py
@@ -1,21 +1,63 @@
 # Importação
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "HS1SD12@#sklj112"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
+login_manager = LoginManager()
 db = SQLAlchemy(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+CORS(app)
 
 # modelagem
-# Produto (id, name, price, description)
+# User (id, username, password)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=True)
+
+# Product (id, name, price, description)
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
+# Autentication
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+      
+@app.route('/login', methods=["POST"])
+def login():
+    # Receber os dados do usuário
+    # Recuperar esse usuário do banco de dados
+    # Se as credenciais estiverem corretas a autenticação é realizada
+    # Caso contrario retorna resposta "Credenciais Inválidas"   
+    data = request.json
+    
+    user = User.query.filter_by(username=data.get("username")).first()
+    
+    if user:
+        if data.get("password") == user.password:
+            login_user(user)
+            return jsonify({"message": "Logged in successfully"}) 
+        
+    return jsonify({"message": "Unauthorized. Invalid credentials"}), 401
+
+@app.route('/logout', methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout successfully"})
+
 @app.route('/api/products/add', methods=["POST"])
+@login_required
 def add_product():
     data = request.json
     if 'name' in data and 'price' in data:
@@ -26,6 +68,7 @@ def add_product():
     return jsonify({"message": "Invalid product data"}), 400
 
 @app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+@login_required
 def delete_product(product_id):
     # Recuperar o produto da base de dados
     # Verificar se o produto existe
@@ -51,6 +94,7 @@ def get_product_details(product_id):
     return jsonify({"message": "Product not found"}), 404 
 
 @app.route('/api/products/update/<int:product_id>', methods=["PUT"])  
+@login_required
 def update_product(product_id):
     product = Product.query.get(product_id)
     if not product:
@@ -78,7 +122,6 @@ def get_products():
             "id": product.id,
             "name": product.name,
             "price": product.price,
-            "description": product.description
         }
         product_list.append(product_data)
     return jsonify(product_list)    
